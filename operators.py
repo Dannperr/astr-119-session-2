f37feca142cda05cb90aeca637bff6b4097c4daf
Danyouu <43651646+Danyouu@users.noreply.github.com>
2018-10-02 13:26:14 -0700

    Session 2 Exercises

---
+++ operators.py
@@ -0,0 +1,40 @@
+# program to practice operators
+
+x = 9 #make x equal to 9
+y = 3 #make y equal to 3
+
+print(x+y) # addition
+print(x-y) #subtraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x%y) #modulus
+print(x**y) #exponentiation
+x = 9.191823
+print(x//y) #floor division
+
+#assignment operators
+x = 9 #sets x equal to 9
+x += 3 #set x = x + 3
+print(x)
+x = 9
+x-= 3 
+print(x)
+x *= 3
+print(x)
+x /= 3 # x = x / 3
+print(x)
+x **=3 #x = x **3
+print(x)
+
+#comparison Operators
+x = 9
+y = 3
+
+print(x==y)	#True if x equals y, False otherwise
+print(x!=y)	#False if x equals y, True otherwise
+print(x<y)	#True if x is greater than y, False ow
+print(x>y)	#True if x is less than y, False ow
+print(x>=y)	#True if x is greater or equal to y, False ow
+print(x<=y)	#True if x is less than or equal to y, False ow
+
+
